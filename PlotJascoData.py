0a5194ed47971dcb4a8bbf5f5e42adc0983b7a0a
thejameshoward <jrhoward@utexas.edu>
2021-07-23 17:20:38 -0500

    Renamed some files and added more plotting functionality

--- PlotJascoData.py
+++ PlotJascoData.py
@@ -1,13 +1,7 @@
-import pandas as pd
 import math
 import os
 import matplotlib.pyplot as plt
 from JASCOScanFile import JascoScanFile
-
-'''
-removes half wavelengths (198.5 nm) from spectra and reorganizes it from the JASCO spectrometers
-'''
-
 
 def FindCSVs():
     csvs = []
@@ -17,26 +11,27 @@ def FindCSVs():
     return csvs
 
 def PlotCD(JascoScanFile, plotmax=False, ylimits=None, title=True):
-    #Define the scanfile we're working with
+    #Define the scanfile we're working with, get the x and y lists for plotting
     f = JascoScanFile
-
     cd = f.get_CD()
     x, y = list(cd.keys()), list(cd.values())
     
-    
-    
-    #Max CD for scaling axes and plotmax
+    #Max CD for scaling Y axis and plotmax
     maxcd = f.get_max_CD()
     if ylimits == None:
-        ylim = abs(maxcd[1]*1.1)
+        ylim, ylim2 = -abs(maxcd[1]*1.1), abs(maxcd[1]*1.1)
     elif type(ylimits) == tuple:
-        pass
+        ylim, ylim2 = ylimits[0], ylimits[1]
+        
+    #Wavelengths for scaling X axis
+    xlim, xlim2 = min(x), max(x)
     
     #Create figure fig and add an axis, ax
     fig_CD, ax_CD = plt.subplots(1)
     plt.axhline(y=0, color = '#D1D1D1')
     ax_CD.plot(x,y, color='purple')
-    ax_CD.set_ylim(-ylim, ylim)
+    ax_CD.set_ylim(ylim, ylim2)
+    ax_CD.set_xlim(xlim, xlim2)
     
     #Plotmax=True
     if plotmax == True:
@@ -58,11 +53,15 @@ def PlotCD(JascoScanFile, plotmax=False, ylimits=None, title=True):
     
 def PlotAbsorbance(JascoScanFile):
     f = JascoScanFile
-    
     absorbance = f.get_abs()
     x, y = list(absorbance.keys()), list(absorbance.values())
     
+    #Wavelengths for scaling X axis
+    xlim, xlim2 = min(x), max(x)
+    
     fig_ABS, ax_ABS = plt.subplots(1) #Create figure fig and add an axis, ax
+    ax_ABS.plot(x,y, color='blue')
+    ax_ABS.set_xlim(xlim, xlim2)
     ax_ABS.plot(x,y)
     
     plt.ylabel("Absorbance (au)")
@@ -70,11 +69,9 @@ def PlotAbsorbance(JascoScanFile):
     plt.title(f.name())
     plt.show()
 
+csvs = FindCSVs()
 
-
-d = JascoScanFile("JRH_2074-2-S-PheNA.csv")
-content = d.get_content()
-PlotCD(d, plotmax=True, title=True)
-PlotAbsorbance(d)
-print(d.get_max_CD())
-maxcd = d.get_max_CD()
+for csv in csvs:
+    f = JascoScanFile(csv)
+    PlotCD(f, plotmax=True, title=True, ylimits=(-50, 50))
+    PlotAbsorbance(f)
