174094781b4549a0a7ff3a1cc165952ea8d54d1f
thejameshoward <jrhoward@utexas.edu>
2021-08-07 15:42:14 -0500

    Cleaned up the plotting function to start checkign whether the entry fields are blank

--- test_gui.py
+++ test_gui.py
@@ -25,15 +25,24 @@ class App():
         if self.file == None:
             print("File not selected")
             return 0
-        if self.color_entry.get() == '':
-            self.CDPlot = PlotCD(JascoScanFile(
-                self.file.name), returnfigure=True, plotzeroline=self.plot_zero_line.get())
+
+        ####GETTING THE PLOTTING PARAMETERS#####
+        ###COLOR###
+        if self.color_entry.get() == '': #Blank color_entry yields purple by default
+            color = 'purple'
         else:
-            self.CDPlot = PlotCD(JascoScanFile(self.file.name), returnfigure=True,
-                                 color=str(self.color_entry.get()), plotzeroline=self.plot_zero_line.get())
+            color = str(self.color_entry.get())
+        ###TITLE###
+        if self.title_entry.get() =='': #Blank title entry yields default title in JASCOScanFile
+            title=True
+        else:
+            title=self.title_entry.get()
+        ###YLIMITS###
+        ###XLIMITS###
+        self.CDPlot = PlotCD(JascoScanFile(self.file.name), returnfigure=True, color=str(color), plotzeroline=self.plot_zero_line.get(), title=title)
 
         self.CDPlot = self.CDPlot.get_tk_widget()
-        self.CDPlot.grid(row=5, column=4)
+        self.CDPlot.grid(row=99, column=4)
 
     def create_PlotCD_button(self):
         self.plot_button = tk.Button(root, text="Plot")
@@ -42,12 +51,14 @@ class App():
 
     def create_left_side_labels(self):
         self.file_label = Label(text="File").grid(row=2, column=0)
-        self.color_label = Label(text="Color").grid(row=3, column=0)
+        self.title_label = Label(text="Title").grid(row=3, column=0)
+        self.color_label = Label(text="Color").grid(row=4, column=0)
 
     def _file_selection(self):
         self.file = filedialog.askopenfile(parent=root, title='Choose a file')
-        if self.file != None:  #If a string is returned from the askopen function (ie file was selected, update file label)
-            self.file_selected_label.config(text = self.file.name.split("/")[-1])
+        # If a string is returned from the askopen function (ie file was selected, update file label)
+        if self.file != None:
+            self.file_selected_label.config(text=self.file.name.split("/")[-1])
         else:
             pass
 
@@ -58,23 +69,30 @@ class App():
 
     def create_select_file_label(self):
         self.filename = "No file selected"
-        self.file_selected_label = tk.Label(root, text=self.filename, wraplength=120)
+        self.file_selected_label = tk.Label(
+            root, text=self.filename, wraplength=120)
         self.file_selected_label.grid(row=2, column=1)
+
+    def create_title_entry(self):
+        self.title_entry = tk.Entry(relief=SUNKEN)
+        self.title_entry.focus()
+        self.title_entry.grid(row=3, column=1)
 
     def create_color_entry(self):
         self.color_entry = tk.Entry(relief=SUNKEN)
         self.color_entry.focus()
-        self.color_entry.grid(row=3, column=1)
+        self.color_entry.grid(row=4, column=1)
 
     def create_plot_zero_line(self):
         self.plot_zero_line = IntVar()
         self.Checkbutton = tk.Checkbutton(
             root, text="Plot 0 line", variable=self.plot_zero_line)
-        self.Checkbutton.grid(row=4, column=1)
+        self.Checkbutton.grid(row=10, column=1)
 
     def run(self):
         self.create_select_file_button()
         self.create_select_file_label()
+        self.create_title_entry()
         self.create_color_entry()
         self.create_PlotCD_button()
         self.create_plot_zero_line()
